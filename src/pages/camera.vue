dce832c35bdbdc0c3827c586449a371bae698afd
mizuki.yasuda <mizuki.yasuda@bigtreetc.com>
2020-11-06 15:18:40 +0900

    feat: save photo

--- src/pages/camera.vue
+++ src/pages/camera.vue
@@ -1,11 +1,15 @@
 <template>
   <q-page class="flex flex-center">
-    <h1>This is SIGNATURE page</h1>
+    <h1>This is CAMERA page</h1>
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-full.svg"
     >
     <video ref="video" id="video"  autoplay playsinline ></video>
+    <q-btn @click="takePhoto">TAKE PHOTO!</q-btn>
+    <img :src="imgSrc" />
+    <q-btn @click="downloadPhoto">DOWNLOAD PHOTO!</q-btn>
+    <a id="imgatag" download="photo.png" :href="imgSrc"/>
   </q-page>
 </template>
 
@@ -14,7 +18,8 @@ export default {
   name: 'app',
   data () {
     return {
-      video: {}
+      video: {},
+      imgSrc: ''
     }
   },
   mounted () {
@@ -33,6 +38,25 @@ export default {
         console.log(err)
       })
     }
-  }
+  },
+  methods: {
+    takePhoto() {
+      const self = this
+      const track = this.video.srcObject.getVideoTracks()[0]
+      const imageCapture = new ImageCapture(track)
+      imageCapture.takePhoto().then(function(blob) {
+        console.log('Took photo:', blob);
+        // img.classList.remove('hidden');
+        self.imgSrc = URL.createObjectURL(blob);
+      }).catch(function(error) {
+        console.log('takePhoto() error: ', error);
+      });
+    },
+    downloadPhoto() {
+      // window.navigator.msSaveBlob(this.imgSrc, "photo.png");
+      // document.getElementById("download").href = window.URL.createObjectURL(this.imgSrc); 
+      document.getElementById("imgatag").click()
+    }
+  },
 }
 </script>
